db07d778f21b91d6bff14f14027ee7ecab7801f3
slowyan <yan@LAPTOP-T2UU0F1D>
2023-01-30 12:54:17 +0300

    install eslint and made code to airbnb

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 const userName = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
-export default userName; 
+export default userName;
