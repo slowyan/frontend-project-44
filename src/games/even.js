fe776aa9473aec274f69290fa0c41272e7ee9efd
slowyan <yan@LAPTOP-T2UU0F1D>
2023-02-06 23:41:38 +0300

    fix mistake in brain-even

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import generateRandomNumber from '../getRandomNumber.js';
 import startGame from '../index.js';
 
-const isEven = (num) => (num % 2 === 0 ? 'no' : 'yes');
+const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const playIsEven = () => {
