64582dff3382165487999ce12961b5b390eff08a
slowyan <yan@LAPTOP-T2UU0F1D>
2023-02-10 13:14:19 +0300

    change findGcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,7 +4,7 @@ import startGame from '../index.js';
 const findGcd = (n1, n2) => {
   let num1 = n1;
   let num2 = n2;
-  while (num1 !== 0 || num2 !== 0) {
+  while (num1 !== 0 && num2 !== 0) {
     if (num1 > num2) {
       num1 %= num2;
     } else {
@@ -18,8 +18,8 @@ const description = 'Find the greatest common divisor of given numbers.';
 
 const playFindGcd = () => {
   const buildDataGame = () => {
-    const num1 = generateRandomNumber(2, 99);
-    const num2 = generateRandomNumber(2, 99);
+    const num1 = generateRandomNumber(0, 101);
+    const num2 = generateRandomNumber(0, 101);
     const question = `${num1} ${num2}`;
     const rightAnswer = String(findGcd(num1, num2));
     return [question, rightAnswer];
